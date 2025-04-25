b650660d0d0b9a76df478a4eda8184a60b99112f
Ezequiel-B18 <eablajevitch@gmail.com>
2025-04-25 10:08:43 -0300

    Implementacion de validaciones en las 3 clases

--- src/profesor.py
+++ src/profesor.py
@@ -3,6 +3,8 @@ from src.persona import Persona
 class Profesor(Persona):
     def __init__(self, nombre, apellido, dni, sueldo):
         super().__init__(nombre, apellido, dni)
+        if sueldo < 0:
+            raise ValueError("El sueldo no puede ser negativo")
         self.sueldo = sueldo
 
     def __repr__(self):
