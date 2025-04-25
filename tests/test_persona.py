dbf72fd740a319a003319e993dbfc4e6fffb82a1
Ezequiel-B18 <eablajevitch@gmail.com>
2025-04-25 09:11:05 -0300

    Tests para el metodo pensar

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -13,3 +13,13 @@ class TestPersona(unittest.TestCase):
         expected = "Persona: DNI: 12345678, Nombre: Juan, Apellido: Perez, Ultima Idea:<No penso en nada>"
         self.assertEqual(str(persona), expected)
 
+    def test_pensar_incrementar_contador(self):
+        persona = Persona("Juan","Perez" ,"12345678")
+        persona.pensar("Hola mundo")
+        self.assertEqual(persona.pensamientos, 1)
+
+    def test_pensar_actualiza_ultima_idea(self):
+        persona = Persona("Juan","Perez" ,"12345678")
+        persona.pensar("Hola mundo")
+        self.assertEqual(persona.ultima_idea, "Hola mundo")
+
